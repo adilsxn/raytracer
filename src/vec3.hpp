5ecdd75250ebbae97d8dc231c24bbb16d8c2c82d
adilsxn <24adilsonnunes@gmail.com>
2025-09-02 22:33:33 +0100

    new scalar prod, dot prod, and the length of vector plus constexpr

--- src/vec3.hpp
+++ src/vec3.hpp
@@ -1,25 +1,40 @@
-#ifndef VEC3
-#define VEC3
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
 
 class vec3 {
   double x;
   double y;
   double z;
 
-  // TODO: Add all the missing operations dot prod, sum, subtration
 public:
-  vec3() = default;
-  vec3(double x, double y, double z) : x{x}, y{y}, z{z} {};
-  // auto operator<=>(const vec3 &) const = default;
-  vec3 scalarMult(int scalar) {
-    return vec3{x * scalar, y * scalar, z * scalar};
+  explicit constexpr vec3(double val): x{val}, y{val}, z{val} {};
+  explicit constexpr vec3(double x, double y, double z) : x{x}, y{y}, z{z} {};
+  vec3 operator+(vec3 rhs) const { return vec3{x + rhs.x, y + rhs.y, z - rhs.z}; }
+  vec3 operator-(vec3 rhs) const { return vec3{x - rhs.x, y - rhs.y, z - rhs.z}; }
+  vec3 operator-=(vec3& rhs ) {
+      x -= rhs.x;
+      y -= rhs.y;
+      z -= rhs.z;
+      return *this;
   }
-  vec3 operator+(vec3 rhs) { return vec3{x + rhs.x, y + rhs.y, z - rhs.z}; }
-
-  vec3 operator-(vec3 rhs) { return vec3{x - rhs.x, y - rhs.y, z - rhs.z}; }
-  double operator*(vec3 rhs) {
-    return this->x * rhs.x + this->y * rhs.y + this->z * rhs.z;
+  vec3 operator+=(vec3& rhs ) {
+      x += rhs.x;
+      y += rhs.y;
+      z += rhs.z;
+      return *this;
+  }
+  auto length()->int{
+      return std::sqrt(x*x + y*y + z*z);
+  }
+  friend constexpr auto dot(vec3 a, vec3 b) -> double {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+  }
+  constexpr friend auto  scalarMult(int scalar, vec3 vec) -> vec3 {
+    return vec3{vec.x * scalar, vec.y * scalar, vec.z * scalar};
   }
 };
 
-#endif // !VEC3
+
+#endif // !VEC3_H
