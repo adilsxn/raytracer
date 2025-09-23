48da226bc5d07f309d1abeb7e68522c32b4b5b38
adilsxn <24adilsonnunes@gmail.com>
2025-09-23 21:49:05 +0100

    fix typos on vec3 type

--- src/vec3.hpp
+++ src/vec3.hpp
@@ -4,37 +4,46 @@
 #include <cmath>
 
 class vec3 {
-  double x;
-  double y;
-  double z;
+  double _x;
+  double _y;
+  double _z;
 
 public:
-  explicit constexpr vec3(double val) : x{val}, y{val}, z{val} {};
-  explicit constexpr vec3(double x, double y, double z) : x{x}, y{y}, z{z} {};
+  explicit constexpr vec3(double val) : _x{val}, _y{val}, _z{val} {};
+  explicit constexpr vec3(double x, double y, double z)
+      : _x{x}, _y{y}, _z{z} {};
   vec3 operator+(vec3 rhs) const {
-    return vec3{x + rhs.x, y + rhs.y, z - rhs.z};
+    return vec3{_x + rhs._x, _y + rhs._y, _z + rhs._z};
   }
-  vec3 operator-(vec3 rhs) const {
-    return vec3{x - rhs.x, y - rhs.y, z - rhs.z};
+
+  constexpr vec3 operator-(vec3 rhs) const {
+    return vec3{_x - rhs._x, _y - rhs._y, _z - rhs._z};
   }
-  vec3 operator-=(vec3 &rhs) {
-    x -= rhs.x;
-    y -= rhs.y;
-    z -= rhs.z;
+  constexpr vec3 operator-=(vec3 &rhs) {
+    _x -= rhs._x;
+    _y -= rhs._y;
+    _z -= rhs._z;
     return *this;
   }
-  vec3 operator+=(vec3 &rhs) {
-    x += rhs.x;
-    y += rhs.y;
-    z += rhs.z;
+  constexpr vec3 operator+=(vec3 &rhs) {
+    _x += rhs._x;
+    _y += rhs._y;
+    _z += rhs._z;
     return *this;
   }
-  auto length() -> int { return std::sqrt(x * x + y * y + z * z); }
-  friend constexpr auto dot(vec3 a, vec3 b) -> double {
-    return a.x * b.x + a.y * b.y + a.z * b.z;
+  constexpr auto length() -> int {
+    return std::sqrt(_x * _x + _y * _y + _z * _z);
   }
-  constexpr friend auto scalarMult(int scalar, vec3 vec) -> vec3 {
-    return vec3{vec.x * scalar, vec.y * scalar, vec.z * scalar};
+
+  constexpr auto dot(vec3 b) -> double {
+    return _x * b._x + _y * b._y + _z * b._z;
+  }
+
+  constexpr auto scalar(int scalar) -> vec3 {
+    return vec3{_x * scalar, _y * scalar, _z * scalar};
+  }
+  constexpr auto norma() -> vec3 {
+    return vec3{_x / length(), _y / length(), _z / length()};
   }
 };
 
