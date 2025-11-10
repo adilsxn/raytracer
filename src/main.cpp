c2d6e2a99b039b4d0f2ba39b5d2207e3f83d63a5
adilsxn <24adilsonnunes@gmail.com>
2025-11-10 21:50:13 +0000

    trying to preview ray directions as colors, hoping for a gradient

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
-#include "macros.hpp"
 #include "ray.hpp"
 #include "vec3.hpp"
-#include <cmath>
+#include <algorithm>
 #include <print>
 
 auto putPixelAt(const ray &r) -> void {
-  std::print("{} {} {}\n", std::fmod(r._dir._x, 256.0),
-             std::fmod(r._dir._y, 256.0), 0);
+  // std::print("{} {} {}\n", r._dir._x, r._dir._y, 0.0);
+  auto rc =
+      static_cast<int>(255.0 * std::clamp((r._dir._x + 1.0) * 0.5, 0.0, 1.0));
+  auto gc =
+      static_cast<int>(255.0 * std::clamp((r._dir._y + 1.0) * 0.5, 0.0, 1.0));
+  auto bc =
+      static_cast<int>(255.0 * std::clamp((r._dir._z + 1.0) * 0.5, 0.0, 1.0));
+  std::print("{} {} {}\n", rc, gc, bc);
 }
 
 // auto makeCirclePPM() -> void {
@@ -27,26 +32,26 @@ auto putPixelAt(const ray &r) -> void {
 // }
 
 auto main() -> int {
-  constexpr auto width = WIDTH;
-  constexpr auto height = HEIGHT;
+  constexpr auto width = 960;
+  constexpr auto height = 720;
   auto top_left = vec3{-1, 0.75, 0};
   auto top_right = vec3{1, 0.75, 0};
   auto bot_left = vec3{-1, -0.75, 0};
   auto bot_right = vec3{1, -0.75, 0};
-  auto camera = vec3{0, 0, -1};
+  auto camera = vec3{0, 0, -0.5};
+
+  std::println("P3\n{} {}\n255", width, height);
 
   for (auto x = 0; x < width; x++) {
     for (auto y = 0; y < height; y++) {
-      auto xx = (double)x;
-      auto yy = (double)(height - y - 1);
+      auto xx = (double)x / (width - 1);
+      auto yy = 1.0 - (double)y / (height - 1);
       auto top = vec3::lerp(top_left, top_right, xx);
       auto bot = vec3::lerp(bot_left, bot_right, xx);
-      auto point = vec3::lerp(top, bot, yy);
-      auto rr = ray(camera, (point - camera));
-      std::println("P3\nWIDTH HEIGHT\n255");
+      auto point = vec3::lerp(bot, top, yy);
+      auto dirdir = (point - camera);
+      auto rr = ray(camera, dirdir.normalize());
       putPixelAt(rr);
     }
   }
-
-  std::print("");
 }
